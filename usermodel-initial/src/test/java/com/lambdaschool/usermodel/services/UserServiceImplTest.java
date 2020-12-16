88e5fff80ed4b97b2a6d5a970cefaad101712c63
chaz-kiker <chaz-kiker@lambdastudents.com>
2020-12-16 15:45:35 -0600

    finish UserServiceImpl test methods for everything but update

--- usermodel-initial/src/test/java/com/lambdaschool/usermodel/services/UserServiceImplTest.java
+++ usermodel-initial/src/test/java/com/lambdaschool/usermodel/services/UserServiceImplTest.java
@@ -2,10 +2,12 @@ package com.lambdaschool.usermodel.services;
 
 
 import com.lambdaschool.usermodel.UserModelApplication;
+import com.lambdaschool.usermodel.exceptions.ResourceNotFoundException;
 import com.lambdaschool.usermodel.models.Role;
 import com.lambdaschool.usermodel.models.User;
 import com.lambdaschool.usermodel.models.UserRoles;
 import com.lambdaschool.usermodel.models.Useremail;
+import com.lambdaschool.usermodel.repository.RoleRepository;
 import com.lambdaschool.usermodel.repository.UserRepository;
 import org.junit.After;
 import org.junit.Before;
@@ -19,11 +21,12 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 
 
 @RunWith(SpringRunner.class)
@@ -41,6 +44,8 @@ public class UserServiceImplTest {
 	@MockBean
 	private UserRepository userRepo;
 	@MockBean
+	private RoleRepository roleRepo;
+	@MockBean
 	private UserAuditing   userAuditing;
 
 	@Before
@@ -48,15 +53,15 @@ public class UserServiceImplTest {
 		//		userService.deleteAll();
 		//		roleService.deleteAll();
 		Role r1 = new Role("admin");
+		r1.setRoleid(1);
 		Role r2 = new Role("user");
+		r2.setRoleid(2);
 		Role r3 = new Role("data");
+		r3.setRoleid(3);
 
 		//		r1 = roleService.save(r1);
 		//		r2 = roleService.save(r2);
 		//		r3 = roleService.save(r3);
-		r1.setRoleid(1);
-		r2.setRoleid(2);
-		r3.setRoleid(3);
 
 
 		// admin, data, user
@@ -173,5 +178,298 @@ public class UserServiceImplTest {
 				           .getUsername()
 		);
 	}
+
+	@Test(expected = ResourceNotFoundException.class)
+	public void findUserByIdNotFound() {
+		Mockito.when(userRepo.findById(4L))
+		       .thenReturn(Optional.empty());
+		assertEquals(
+				"admin",
+				userService.findUserById(4)
+				           .getUsername()
+		);
+	}
+
+	@Test
+	public void findByNameContaining() {
+		Mockito.when(userRepo.findByUsernameContainingIgnoreCase("admin"))
+		       .thenReturn(userList);
+		//		System.out.println(userService.findByNameContaining("admin"));
+		assertEquals(
+				3,
+				userService.findByNameContaining("admin")
+				           .size()
+		);
+	}
+
+	@Test
+	public void findByNameContainingNoSuchName() {
+		Mockito.when(userRepo.findByUsernameContainingIgnoreCase("zxoinasodfihgoiweh"))
+		       .thenReturn(new ArrayList<>());
+		assertEquals(
+				0,
+				userService.findByNameContaining("zxoinasodfihgoiweh")
+				           .size()
+		);
+	}
+
+	@Test
+	public void findAll() {
+		Mockito.when(userRepo.findAll())
+		       .thenReturn(userList);
+		assertEquals(
+				3,
+				userService.findAll()
+				           .size()
+		);
+	}
+
+	@Test
+	public void findByName() {
+		Mockito.when(userRepo.findByUsername("admin"))
+		       .thenReturn(userList.get(0));
+		assertEquals(
+				"admin",
+				userService.findByName("admin")
+				           .getUsername()
+		);
+	}
+
+	@Test(expected = ResourceNotFoundException.class)
+	public void findByNameNotFound() {
+		Mockito.when(userRepo.findByUsername("zxoinasodfihgoiweh"))
+		       .thenReturn(null);
+		assertEquals(
+				"zxoinasodfihgoiweh",
+				userService.findByName("zxoinasodfihgoiweh")
+				           .getUsername()
+		);
+	}
+
+	@Test
+	public void delete() {
+		Mockito.when(userRepo.findById(4L))
+		       .thenReturn(Optional.of(userList.get(0)));
+		Mockito.doNothing()
+		       .when(userRepo)
+		       .deleteById(4L);
+		userService.delete(4);
+		assertEquals(
+				3,
+				userList.size()
+		);
+	}
+
+	@Test(expected = ResourceNotFoundException.class)
+	public void deleteFailed() {
+		Mockito.when(userRepo.findById(777L))
+		       .thenReturn(Optional.empty());
+		Mockito.doNothing()
+		       .when(userRepo)
+		       .deleteById(777L);
+		userService.delete(777);
+		assertEquals(
+				3,
+				userList.size()
+		);
+	}
+
+	@Test
+	public void save() {
+		Role r3 = new Role("data");
+		r3.setRoleid(3);
+
+		String u3Name = "barnbarn";
+
+		User u3 = new User(
+				u3Name,
+				"ILuvM4th!",
+				"barnbarn@lambdaschool.local"
+		);
+		//		u3.setUserid(33);
+		u3.getRoles()
+		  .add(new UserRoles(
+				  u3,
+				  r3
+		  ));
+		u3.getUseremails()
+		  .add(new Useremail(
+				  u3,
+				  "barnbarn@email.local"
+		  ));
+
+		Mockito.when(userRepo.save(any(User.class)))
+		       .thenReturn(u3);
+		Mockito.when(roleRepo.findById(3L))
+		       .thenReturn(Optional.of(r3));
+		Mockito.when(roleService.findRoleById(3))
+		       .thenReturn(r3);
+
+		User addedUser = userService.save(u3);
+		assertNotNull(addedUser);
+		assertEquals(
+				u3Name,
+				addedUser.getUsername()
+		);
+
+	}
+
+	//	@Test
+	@Test(expected = ResourceNotFoundException.class)
+	public void saveRoleNotFound() {
+		Role r3 = new Role("data");
+		r3.setRoleid(3333);
+
+		String u3Name = "barnbarn";
+
+		User u3 = new User(
+				u3Name,
+				"ILuvM4th!",
+				"barnbarn@lambdaschool.local"
+		);
+		//		u3.setUserid(33);
+		u3.getRoles()
+		  .add(new UserRoles(
+				  u3,
+				  r3
+		  ));
+		u3.getUseremails()
+		  .add(new Useremail(
+				  u3,
+				  "barnbarn@email.local"
+		  ));
+
+		Mockito.when(userRepo.save(any(User.class)))
+		       .thenReturn(u3);
+		Mockito.when(roleRepo.findById(3333L))
+		       .thenReturn(Optional.empty());
+		Mockito.when(roleService.findRoleById(3333))
+		       .thenThrow(ResourceNotFoundException.class);
+
+		User addedUser = userService.save(u3);
+		assertNotNull(addedUser);
+		assertEquals(
+				u3Name,
+				addedUser.getUsername()
+		);
+	}
+
+	@Test
+	public void savePut() {
+		Role r3 = new Role("data");
+		r3.setRoleid(3);
+
+		String u3Name = "barnbarn";
+
+		User u3 = new User(
+				u3Name,
+				"ILuvM4th!",
+				"barnbarn@lambdaschool.local"
+		);
+		u3.setUserid(33);
+		u3.getRoles()
+		  .add(new UserRoles(
+				  u3,
+				  r3
+		  ));
+		u3.getUseremails()
+		  .add(new Useremail(
+				  u3,
+				  "barnbarn@email.local"
+		  ));
+
+		Mockito.when(userRepo.findById(33L))
+		       .thenReturn(Optional.of(u3));
+		Mockito.when(roleRepo.findById(3L))
+		       .thenReturn(Optional.of(r3));
+		Mockito.when(roleService.findRoleById(3))
+		       .thenReturn(r3);
+
+		Mockito.when(userRepo.save(any(User.class)))
+		       .thenReturn(u3);
+
+		User updatedUser = userService.save(u3);
+		assertNotNull(updatedUser);
+		assertEquals(
+				u3Name,
+				updatedUser.getUsername()
+		);
+	}
+
+	@Test(expected = ResourceNotFoundException.class)
+	public void savePutFail() {
+		Role r3 = new Role("data");
+		r3.setRoleid(3);
+
+		String u3Name = "barnbarn";
+
+		User u3 = new User(
+				u3Name,
+				"ILuvM4th!",
+				"barnbarn@lambdaschool.local"
+		);
+		u3.setUserid(777);
+		u3.getRoles()
+		  .add(new UserRoles(
+				  u3,
+				  r3
+		  ));
+		u3.getUseremails()
+		  .add(new Useremail(
+				  u3,
+				  "barnbarn@email.local"
+		  ));
+
+		Mockito.when(userRepo.findById(777L))
+		       .thenReturn(Optional.empty());
+		Mockito.when(roleRepo.findById(3L))
+		       .thenReturn(Optional.of(r3));
+		Mockito.when(roleService.findRoleById(3))
+		       .thenReturn(r3);
+
+		Mockito.when(userRepo.save(any(User.class)))
+		       .thenReturn(u3);
+
+		User updatedUser = userService.save(u3);
+		assertNotNull(updatedUser);
+		assertEquals(
+				777L,
+				updatedUser.getUserid()
+		);
+	}
+
+//		@Test
+//		public void update() {
+//			Role r3 = new Role("data");
+//			r3.setRoleid(3);
+//
+//			String u3Name = "barnbarn";
+//
+//			User u3 = new User(
+//					u3Name,
+//					"ILuvM4th!",
+//					"barnbarn@lambdaschool.local"
+//			);
+//
+//			u3.setUserid(33);
+//
+//			u3.getRoles()
+//			  .add(new UserRoles(
+//					  u3,
+//					  r3
+//			  ));
+//			u3.getUseremails()
+//			  .add(new Useremail(
+//					  u3,
+//					  "barnbarn@email.local"
+//			  ));
+//		}
+
+	//	@Test(expected = ResourceNotFoundException.class)
+	//	public void updateUserNotFound() {}
+	//
+	//	@Test(expected = ResourceNotFoundException.class)
+	//	public void updateRoleNotFound() {}
+
 
 }
